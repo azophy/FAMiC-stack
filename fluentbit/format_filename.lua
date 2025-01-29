7abf998ea827d01cf259e03f72b833917a73683d
Abdurrahman Shofy Adianto <azophy@gmail.com>
2025-01-29 14:53:47 +0700

    fix (fluentbit): docker log filename format

--- fluentbit/format_filename.lua
+++
@@ -1,9 +0,0 @@
-function format_filename(tag, timestamp, record)
-    -- Extract container name from Docker log path
-    local container_name = string.match(record["container_name"], "([^/]+)$")
-    if container_name then
-        record["container_name"] = container_name
-    end
-    return 2, timestamp, record
-end
-
