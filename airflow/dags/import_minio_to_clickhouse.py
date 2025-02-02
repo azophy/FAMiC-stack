f76cef8e2c2f177e1cb24a5e45ce0ac8853a6ee2
Abdurrahman Shofy Adianto <azophy@gmail.com>
2025-02-03 02:50:04 +0700

    fix task error

--- airflow/dags/import_minio_to_clickhouse.py
+++ airflow/dags/import_minio_to_clickhouse.py
@@ -5,7 +5,7 @@ import pendulum
 
 from airflow.decorators import dag, task
 @dag(
-    schedule="* * * * *",
+    schedule="* * * * *", # run every minutes
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
     tags=["minio", "docker", "clickhouse"],
@@ -14,7 +14,7 @@ def import_minio_to_clickhouse():
     """
     ### Import Docker Logfiles from Minio into Clickhouse with TaskFlow API
     This is a simple data pipeline example. The repository is in
-    [github.com/azophy/docker-minio-airflow-clickhouse-example](https://github.com/azophy/docker-minio-airflow-clickhouse-example)
+    [github.com/azophy/docker-airflow-clickhouse-example](https://github.com/azophy/docker-minio-airflow-clickhouse-example)
     """
     @task.virtualenv(requirements=["clickhouse-connect", "minio"], system_site_packages=False)
     def create_tables():
@@ -42,24 +42,18 @@ def import_minio_to_clickhouse():
         return start_after
 
     @task.virtualenv(requirements=["clickhouse-connect", "minio"], system_site_packages=False)
-    def list_latest_docker_logfiles(start_after):
-        """
-        #### List latest docker logfiles
-        """
-        import common.minio_operations as minio
-
-        minio_client = minio.get_client()
-        return minio.list_docker_logs(minio_client, start_after=start_after);
-
-    @task.virtualenv(requirements=["clickhouse-connect", "minio"], system_site_packages=False)
-    def load_to_clickhouse(log_list):
+    def load_to_clickhouse(start_after):
         """
         #### Load to clickhouse task
         Load all listed docker logfiles into clickhouse
         """
         import common.clickhouse_operations as clickhouse
+        import common.minio_operations as minio
 
         ch_client = clickhouse.get_client()
+        minio_client = minio.get_client()
+        log_list = minio.list_docker_logs(minio_client, start_after=start_after);
+
         for obj in log_list:
             print(obj.object_name, obj.last_modified, obj.size)
             try:
@@ -71,8 +65,7 @@ def import_minio_to_clickhouse():
     create_step = create_tables()
     start_after = get_latest_docker_log_migration()
     create_step >> start_after
-    log_list = list_latest_docker_logfiles(start_after)
-    load_to_clickhouse(log_list)
+    load_to_clickhouse(start_after)
 
 import_minio_to_clickhouse()
 
